748b9f3834de43e996fa10649dceeb7a53639cd3
ObrynKLukongajnr <lukonga.obryn@gmail.com>
2023-07-14 05:26:49 -0700

    Shell scripting file

---
+++ redProg-practice/hellothere.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FIRST_NAME=Obryn
+LAST_NAME=Lukonga
+
+ echo Hello $FIRST_NAME $LAST_NAME
---
+++ redProg-practice/interactiveshell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Asking user for input
+
+echo What is your fisrt name?
+read FIRST_NAME
+echo What is your last name?
+read LAST_NAME
+
+echo Hello $FIRST_NAME $LAST_NAME
